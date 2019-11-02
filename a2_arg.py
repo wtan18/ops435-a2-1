f1875be43a11ac176d691e7e3187c4d9d4ab4c56
Raymond Chan <ray.freeping@gmail.com>
2019-11-02 11:21:37 -0400

    Updated for Fall 2019
    Type of report limited to daily or weekly

--- a2_arg.py
+++ a2_arg.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-
+'''Demonstrate the use of the argparse module: how to retrieve the command line argument.'''
 import argparse
 
 parser = argparse.ArgumentParser(description="Usage Report based on the last command",epilog="Copyright 2018 - Raymond Chan")
 parser.add_argument("-l", "--list", type=str, choices=['user','host'], help="generate user name or remote host IP from the given files")
 parser.add_argument("-r", "--rhost", help="usage report for the given remote host IP")
-parser.add_argument("-t","--type", type=str, choices=['daily','weekly','monthly'], help="type of report: daily, weekly, and monthly")
+parser.add_argument("-t","--type", type=str, choices=['daily','weekly'], help="type of report: daily or weekly")
 parser.add_argument("-u", "--user", help="usage report for the given user name")
 parser.add_argument("-v","--verbose", action="store_true",help="turn on output verbosity")
 parser.add_argument("files",metavar='F', type=str, nargs='+',help="list of files to be processed")
